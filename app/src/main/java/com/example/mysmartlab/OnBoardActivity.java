91c033d65f4fd25a5d54643c4ab1fac67c244346
Jamila <rammv4117@gmail.com>
2023-04-08 14:01:13 +0300

    Session1

---
+++ app/src/main/java/com/example/mysmartlab/OnBoardActivity.java
@@ -0,0 +1,76 @@
+package com.example.mysmartlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class OnBoardActivity extends AppCompatActivity {
+
+    ViewPager slideViewPager;
+    LinearLayout dotIndicator;
+    TextView nextButton , finishButton;
+     OnBoardAdapter viewPagerAdapter;
+    ViewPager.OnPageChangeListener viewPagerListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+        @Override
+        public void onPageSelected(int position) {
+
+            if (position > 0) {
+                nextButton.setText("Пропустить") ;
+            }
+            if (position == 2){
+                nextButton.setText("Завершить");
+            } else {
+                nextButton.setText("Пропустить");
+            }
+        }
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board);
+        finishButton = findViewById(R.id.textView);
+        nextButton = findViewById(R.id.textView);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getItem(0) > 0) {
+                    slideViewPager.setCurrentItem(getItem(-1), true);
+                }
+
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getItem(0) < 2)
+                    slideViewPager.setCurrentItem(getItem(1), true);
+                else {
+                    Intent i = new Intent(OnBoardActivity.this,  RegistrationActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
+        slideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+
+        viewPagerAdapter = new OnBoardAdapter(this);
+        slideViewPager.setAdapter(viewPagerAdapter);
+
+        slideViewPager.addOnPageChangeListener(viewPagerListener);
+    }
+    private int getItem(int i) {
+        return slideViewPager.getCurrentItem() + i;
+    }
+}
---
+++ app/src/main/java/com/example/mysmartlab/OnBoardAdapter.java
@@ -0,0 +1,74 @@
+package com.example.mysmartlab;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class OnBoardAdapter extends PagerAdapter {
+    Context context;
+
+    public OnBoardAdapter(Context context) {
+        this.context = context;
+    }
+    int sliderAllImages[] = {
+            R.drawable.img_3,
+            R.drawable.img_4,
+            R.drawable.img_5,
+
+    };
+    int svipe [] = {
+
+            R.drawable.img_2,
+            R.drawable.img_6,
+            R.drawable.img_7,
+
+    };
+    int sliderAllTitle[] = {
+            R.string.screen1,
+            R.string.screen2,
+            R.string.screen3,
+    };
+    int sliderAllDesc[] = {
+            R.string.screen1desc,
+            R.string.screen2desc,
+            R.string.screen3desc,
+    };
+    @Override
+    public int getCount() {
+        return sliderAllImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.onboatd_model,container,false);
+        ImageView sliderImage = (ImageView) view.findViewById(R.id.imageView3);//1 картинка
+        ImageView slider = (ImageView) view.findViewById(R.id.imageView2); //2 картинка
+        TextView sliderTitle = (TextView) view.findViewById(R.id.textView5); //1
+        TextView sliderDesc = (TextView) view.findViewById(R.id.textView6);
+        sliderImage.setImageResource(sliderAllImages[position]);//2
+        slider.setImageResource(svipe [position]);
+        sliderTitle.setText(this.sliderAllTitle[position]);
+        sliderDesc.setText(this.sliderAllDesc[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
